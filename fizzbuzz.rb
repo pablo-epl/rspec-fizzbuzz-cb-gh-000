77bb7383fd02eee1e632fc4ed2fdf71844190357
Pablo Peña <pablo@makeitworth.com.mx>
2018-05-06 08:20:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(user_input)
-  if user_input%0 and user_input == 5:
+  if user_input % 5 == 0 and user_input == 5:
     return "Buzz"
-  elsif user_input%0 and user_input == 3:
+  elsif user_input % 3 == 0 and user_input == 3:
     return "Fizz"
-  elsif user_input%0 and (user_input == 5 or user_input == 3):
+  elsif user_input % 5 == 0 or user_input % 3 == 0):
     return "FizzBuzz"
   else
     return nil
